f3e4e267a2fa626f6ddcf551696c7c164e991edb
xFIREx2005 <kaique.santos150@etec.sp.gov.br>
2021-05-04 07:37:29 -0300

    conclusao do trabalho

--- javascript/constantes.js
+++ javascript/constantes.js
@@ -2,22 +2,96 @@
 const COLUNA = 10;
 const LINHA = 20;
 const TAM_BLOCO = 30;
-
+const LINHAS_POR_NIVEL = 10;
 
 
 const KEY = {
+  ESC: 27,
   SPACE: 32,
   LEFT: 37,
+  UP: 38,
   RIGHT: 39,
-  DOWN: 40
+  DOWN: 40,
+  P: 80
 }
 Object.freeze(KEY);
 
-const movimento = {
-  [KEY.SPACE]: p => ({ ...p, y: p.y + 1 }),
-  [KEY.LEFT]:  p => ({ ...p, x: p.x - 1 }),
-  [KEY.RIGHT]: p => ({ ...p, x: p.x + 1 }),
-  [KEY.DOWN]:    p => ({ ...p, y: p.y + 1 })
-  
-};
-	
+
+
+const CORES = [
+	'cyan',
+	'blue',
+	'orange',
+	'yellow',
+	'green',
+	'purple',
+	'red'
+];
+Object.freeze(CORES);
+
+const FORMAS = [
+	[],
+	[	[0,0,0,0],
+		[1,1,1,1],
+		[0,0,0,0],
+		[0,0,0,0]],
+		
+	[	[2,0,0],
+		[2,2,2],
+		[0,0,0]],
+		
+	[	[0,0,3],
+		[3,3,3],
+		[0,0,0]],
+		
+	[	[4,4],
+		[4,4]],
+		
+	[	[0,5,5],
+		[5,5,0],
+		[0,0,0]],
+		
+	[	[0,6,0],
+		[6,6,6],
+		[0,0,0]],
+		
+	[	[7,7,0],
+		[0,7,7],
+		[0,0,0]]
+];
+Object.freeze(FORMAS); 
+
+const NIVEL = {
+	0: 800,
+	1: 720,
+	2: 630,
+	3: 550,
+	4: 470,
+	5: 380,
+	6: 300,
+	7: 220,
+	8: 130,
+	9: 100,
+	10: 80,
+	11: 80,
+	12: 80,
+	13: 70,
+	14: 70,
+	15: 70,
+	16: 50,
+	17: 50,
+	18: 50,
+	19: 30,
+	20: 30,
+}
+Object.freeze(NIVEL);
+
+const PONTOS={
+	SIMPLES: 100,
+	DUPLO: 300,
+	TRIPLO: 500,
+	TETRIS: 800,
+	SOFT: 1,
+	DESPENCA: 2,
+}
+Object.freeze(PONTOS);
--- javascript/pecas.js
+++ javascript/pecas.js
@@ -1,18 +1,24 @@
 class Peca {
-	
+	x;
+	y;
+	cor;
+	forma;
+	ctx;
+	idTipo;
 	
 	constructor(ctx){
 		this.ctx = ctx;
-		this.color = 'blue';
-		this.shape = [
-			[2, 0, 0],
-			[2, 2, 2],
-			[0, 0, 0]
-		];
-		
-		this.x = 3;
+		this.spawn();
+	}
+	
+	spawn(){
+		this.idTipo = this.randomizePeca(CORES.length - 1);
+		this.shape = FORMAS[this.idTipo];
+		this.color = CORES[this.idTipo];
+		this.x = 0;
 		this.y = 0;
 	}
+	
 	desenho(){
 		this.ctx.fillStyle = this.color;
 		this.shape.forEach((row, y) => {
@@ -28,7 +34,16 @@ class Peca {
 	mover(p) {
 		this.x = p.x;
 		this.y = p.y;
+		this.shape = p.shape;
 	}
 	
+	setPosicaoInicial(){
+		this.x = this.typeId === 4 ? 4 : 3;
+	}
+	
+	randomizePeca(noDeTipos){
+		return Math.floor(Math.random() * noDeTipos + 1);
+	}
+		
 }
 
